038ad504d50c2fec266b49623e4b5a92af88a528
Your Name <you@example.com>
2024-10-21 20:16:25 +0300

    Added fifth task first lecture

--- Lesson_1/Task_5/main.cpp
+++ Lesson_1/Task_5/main.cpp
@@ -4,26 +4,24 @@ using namespace std;
 
 int main()
 {
-     int quantity = 0;
+    int quantity = 0;
     cout << "Enter the height of the Christmas tree!";
     cin >>quantity;
     int height = 0;
-
     for (int height =0; height<quantity; height++){
-
         for (int width =0; width<=(quantity-height); width++){
             cout << " ";
-         }
+        }
         for (int star = 0; star <= height*2;star ++){
             cout << "*";
-         }
-        cout << endl;
+        }
+        cout << endl;     
     }
     for (int stem =0; stem<=(quantity+1); stem++){
         if(stem<(quantity+1)){
             cout << " ";
         }else{
-             cout << "*";
+            cout << "*";
         }
     }
     return 0;
